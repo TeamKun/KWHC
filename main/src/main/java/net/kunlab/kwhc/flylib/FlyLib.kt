015c1688908c46024efa79f9fb5fbb501a055981
Bun133 <bun133.work@gmail.com>
2021-02-11 09:39:31 +0900

    FlyLib追加

---
+++ main/src/main/java/net/kunlab/kwhc/flylib/FlyLib.kt
@@ -0,0 +1,25 @@
+package net.kunlab.kwhc.flylib
+
+import com.flylib.event.MainListener
+import com.flylib.state.State
+import org.bukkit.plugin.java.JavaPlugin
+
+class FlyLib(var plugin: JavaPlugin) {
+    companion object{
+        var instance:FlyLib? = null
+        fun isExist() = instance!=null
+        fun get(): FlyLib? {
+            if(isExist()) return instance
+            throw FlyLibNotReadyException()
+        }
+    }
+
+    init {
+        plugin.server.pluginManager.registerEvents(MainListener.instance, plugin)
+        instance = this
+    }
+
+    var state = State()
+}
+
+class FlyLibNotReadyException:Exception("FlyLib is not ready")
---
+++ main/src/main/java/net/kunlab/kwhc/flylib/MainListener.kt
@@ -0,0 +1,48 @@
+package net.kunlab.kwhc.flylib
+
+import org.bukkit.event.Event
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.event.inventory.InventoryMoveItemEvent
+import kotlin.reflect.KFunction
+import kotlin.reflect.KFunction1
+
+class MainListener : Listener {
+    companion object {
+        var instance: MainListener = MainListener()
+    }
+
+    @EventHandler
+    fun onInventoryClick(e: InventoryClickEvent) {
+        Events.InventoryClickEvent.execute(e)
+    }
+
+    @EventHandler
+    fun onInventoryDrop(e: InventoryMoveItemEvent) {
+        Events.InventoryMoveEvent.execute(e)
+    }
+
+    @EventHandler
+    fun onInventoryDrop(e: InventoryCloseEvent) {
+        Events.InventoryCloseEvent.execute(e)
+    }
+}
+
+enum class Events(val generic_event_name: String) {
+    InventoryClickEvent("InventoryClickEvent"),
+    InventoryCloseEvent("InventoryCloseEvent"),
+    InventoryMoveEvent("InventoryMoveItemEvent");
+    private val register: ArrayList<KFunction1<Event, Unit>> = arrayListOf()
+    fun register(f: KFunction1<Event, Unit>) {
+        register.add(f)
+    }
+
+    fun execute(e: Event) {
+        for (r in register) {
+            r(e)
+        }
+    }
+}
+
