595686bb6cb47411c1dd87c08d08b82525bed902
Bun133 <bun133.work@gmail.com>
2021-02-11 09:41:47 +0900

    Mainクラス追加

--- main/src/main/java/net/kunlab/kwhc/Kwhc.kt
+++ main/src/main/java/net/kunlab/kwhc/Kwhc.kt
@@ -1,18 +1,23 @@
 package net.kunlab.kwhc
 
+import net.kunlab.kwhc.flylib.FlyLib
 import net.kunlab.kwhc.role.COManager
 import net.kunlab.kwhc.role.RoleManager
+import net.kunlab.kwhc.shop.ShopInstance
 import org.bukkit.plugin.java.JavaPlugin
 
 class Kwhc : JavaPlugin() {
     lateinit var roleManager:RoleManager
     lateinit var coManager :COManager
+    lateinit var shop:ShopInstance
 
     override fun onEnable() {
         // Plugin startup logic
         logger.info("[KWHC]System Start UP Now.")
+        FlyLib(this)
         roleManager = RoleManager(this)
         coManager = COManager(this)
+        shop = ShopInstance(this)
     }
 
 
