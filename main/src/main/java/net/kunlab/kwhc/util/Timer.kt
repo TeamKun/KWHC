66b1635861f5d5695106348770da9a63e62baa24
Bun133 <bun133.work@gmail.com>
2021-02-12 19:26:31 +0900

    読みやすいね。

--- main/src/main/java/net/kunlab/kwhc/util/Timer.kt
+++ main/src/main/java/net/kunlab/kwhc/util/Timer.kt
@@ -2,25 +2,25 @@ package net.kunlab.kwhc.util
 
 import net.kunlab.kwhc.Kwhc
 
-class Timer(val plugin:Kwhc) {
+class Timer(val plugin: Kwhc) {
     init {
         plugin.server.scheduler.runTaskTimer(plugin, Runnable { onTick() }, 1, 1)
     }
 
-    private val queue = mutableMapOf<Runnable,Long>()
+    private val queue = mutableMapOf<Runnable, Long>()
 
     fun onTick() {
         val toRemove = mutableListOf<Runnable>()
         queue.filter { it.value <= 0 }.forEach { it.key.run();toRemove.add(it.key) }
         toRemove.forEach { queue.remove(it) }
-        queue.forEach{ queue[it.key] = queue[it.key]!! - 1 }
+        queue.forEach { queue[it.key] = queue[it.key]!! - 1 }
     }
 
-    fun register(r:Runnable,time:Long){
-        queue[r]=time
+    fun register(r: Runnable, time: Long) {
+        queue[r] = time
     }
 
-    fun remove(r:Runnable){
+    fun remove(r: Runnable) {
         queue.remove(r)
     }
 }
